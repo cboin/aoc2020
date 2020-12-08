2be4436ad01186d48269bb4dfef6e7b334062af3
cboin <clem.boin@gmail.com>
2020-12-08 13:36:26 +0100

    day7

--- src/main.rs
+++ src/main.rs
@@ -4,34 +4,75 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let inputs = parse_inputs();
+    let rules = parse_inputs();
     let target = "shiny gold";
-    let mut matches = HashSet::new();
+    let mut luggages = HashSet::new();
 
-    for (key, values) in inputs.iter() {
-        if contains(values, &target) {
-            matches.insert(key);
+    for key in rules.keys() {
+        if contains(target, &rules, &key) {
+            luggages.insert(key);
         }
     }
 
-    println!("{:?}", matches.len());
+    println!("[part1] {}", luggages.len());
+
+    let mut hm_count: HashMap<String, usize> = HashMap::new();
+    let count = count(target, &mut hm_count, &rules);
+    println!("[part2] {}", count - 1);
 }
 
-fn contains(values: &Option<Vec<(u32, String)>>, target: &str) -> bool {
-    match values {
-        Some(values) => {
-            let values = values
+fn count(
+    target: &str,
+    hm: &mut HashMap<String, usize>,
+    rules: &HashMap<String, Option<Vec<(usize, String)>>>,
+) -> usize {
+    match hm.get(target) {
+        Some(v) => *v,
+        None => {
+            let values = match rules.get(target) {
+                Some(values) => values,
+                None => panic!("no values"),
+            };
+
+            let values = match values {
+                Some(values) => values,
+                None => return 1,
+            };
+
+            let count = values
                 .iter()
-                .map(|(_, r)| r.to_string())
-                .collect::<Vec<String>>();
+                .map(|t| t.0 * count(&t.1, hm, rules))
+                .sum::<usize>()
+                + 1;
 
-            values.iter().any(|x| x == target)
+            hm.insert(target.to_string(), count);
+
+            count
         }
-        None => false,
     }
 }
 
-fn parse_inputs() -> HashMap<String, Option<Vec<(u32, String)>>> {
+fn contains(
+    target: &str,
+    hm: &HashMap<String, Option<Vec<(usize, String)>>>,
+    key: &String,
+) -> bool {
+    let values = match hm.get(key) {
+        Some(values) => values,
+        None => panic!("luggage rules not found"),
+    };
+
+    let values = match values {
+        Some(values) => values,
+        None => return false,
+    };
+
+    values
+        .iter()
+        .any(|x| x.1 == target || contains(&target, &hm, &x.1))
+}
+
+fn parse_inputs() -> HashMap<String, Option<Vec<(usize, String)>>> {
     let f = match File::open("inputs/day7") {
         Ok(f) => BufReader::new(f),
         Err(e) => panic!("{}", e),
@@ -64,20 +105,22 @@ fn parse_inputs() -> HashMap<String, Option<Vec<(u32, String)>>> {
                         };
 
                         let rule = rule.split_at(position);
-                        let number = match rule.0.to_string().parse::<u32>() {
+                        let number = match rule.0.to_string().parse::<usize>() {
                             Ok(number) => number,
                             Err(e) => panic!("{}", e),
                         };
 
                         (number, rule.1.trim().to_string())
                     })
-                    .collect::<Vec<(u32, String)>>();
+                    .collect::<Vec<(usize, String)>>();
 
                 (luggage, Some(rules))
             }
             Err(e) => panic!("{}", e),
         })
-        .collect::<Vec<(String, Option<Vec<(u32, String)>>)>>();
+        .collect::<Vec<(String, Option<Vec<(usize, String)>>)>>();
 
-    inputs.into_iter().collect::<HashMap<String, Option<_>>>()
+    inputs
+        .into_iter()
+        .collect::<HashMap<String, Option<Vec<(usize, String)>>>>()
 }
